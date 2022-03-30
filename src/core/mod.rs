f6dfb6c9288419cb3eca839c6a368a87c3ff7597
Alexey Gerasev <alexey.gerasev@gmail.com>
2022-03-30 14:36:09 +0700

    Core module

---
+++ src/core/mod.rs
@@ -0,0 +1,10 @@
+pub trait Agent {
+    type Input;
+    type Output;
+
+    fn process(&mut self, input: Self::Input) -> Self::Output;
+}
+
+pub trait System {
+    fn process(&mut self);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 #![forbid(unsafe_code)]
 
 pub mod app;
+pub mod core;
