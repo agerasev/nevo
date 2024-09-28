3bf48cd6fd3815f25318035e352e22967765507f
Alexey Gerasev <alexey.gerasev@gmail.com>
2024-09-28 16:38:50 +0700

    Determined tensor initialization

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -15,6 +15,20 @@ pub trait Agent {
     fn process<C: Context>(&mut self, cx: &mut C, input: Self::Input) -> Self::Output;
 }
 
+pub trait Genome: Clone + Sized {
+    fn mutate<R: Rng + ?Sized>(&mut self, rate: f64, rng: &mut R);
+}
+
+pub trait Sexual: Genome {
+    fn recombine<R: Rng + ?Sized>(&self, other: &Self, rng: &mut R) -> Option<Self>;
+}
+
+pub trait Evolving {
+    type Genome: Genome;
+    fn genome(&self) -> Self::Genome;
+    fn instance<C: Context>(genome: &Self::Genome, cx: &mut C) -> Self;
+}
+
 pub trait System {
     fn step<C: Context>(&mut self, cx: &mut C);
 }
---
+++ nn/src/init.rs
@@ -0,0 +1,179 @@
+use std::{
+    ops::{Deref, DerefMut},
+    sync::{Arc, Mutex},
+};
+
+use candle::{DType, Device, Error, Result, Shape, Tensor, Var, WithDType};
+use candle_nn::{
+    init::{self, NormalOrUniform},
+    var_builder::SimpleBackend,
+    VarMap,
+};
+use half::{bf16, f16};
+use rand::Rng;
+use rand_distr::{Distribution, Normal, Uniform};
+
+pub fn rand<T: WithDType, R: Rng + ?Sized, D: Distribution<T>>(
+    rng: &mut R,
+    shape: &Shape,
+    device: &Device,
+    distr: D,
+) -> Result<Tensor> {
+    Tensor::from_iter(rng.sample_iter(distr).take(shape.elem_count()), device)?.reshape(shape)
+}
+
+pub fn rand_uniform_f64<R: Rng + ?Sized>(
+    rng: &mut R,
+    shape: &Shape,
+    dtype: DType,
+    device: &Device,
+    min: f64,
+    max: f64,
+) -> Result<Tensor> {
+    match dtype {
+        DType::U8 | DType::U32 | DType::I64 => {
+            Err(Error::UnsupportedDTypeForOp(dtype, "rand_uniform").bt())
+        }
+        DType::BF16 => rand(
+            rng,
+            shape,
+            device,
+            Uniform::new(bf16::from_f64(min), bf16::from_f64(max)),
+        ),
+        DType::F16 => rand(
+            rng,
+            shape,
+            device,
+            Uniform::new(f16::from_f64(min), f16::from_f64(max)),
+        ),
+        DType::F32 => rand(rng, shape, device, Uniform::new(min as f32, max as f32)),
+        DType::F64 => rand(rng, shape, device, Uniform::new(min, max)),
+    }
+}
+
+pub fn rand_normal_f64<R: Rng + ?Sized>(
+    rng: &mut R,
+    shape: &Shape,
+    dtype: DType,
+    device: &Device,
+    mean: f64,
+    std: f64,
+) -> Result<Tensor> {
+    match dtype {
+        DType::U8 | DType::U32 | DType::I64 => {
+            Err(Error::UnsupportedDTypeForOp(dtype, "rand_uniform").bt())
+        }
+        DType::BF16 => rand(
+            rng,
+            shape,
+            device,
+            Normal::new(bf16::from_f64(mean), bf16::from_f64(std)).map_err(Error::wrap)?,
+        ),
+        DType::F16 => rand(
+            rng,
+            shape,
+            device,
+            Normal::new(f16::from_f64(mean), f16::from_f64(std)).map_err(Error::wrap)?,
+        ),
+        DType::F32 => rand(
+            rng,
+            shape,
+            device,
+            Normal::new(mean as f32, std as f32).map_err(Error::wrap)?,
+        ),
+        DType::F64 => rand(
+            rng,
+            shape,
+            device,
+            Normal::new(mean, std).map_err(Error::wrap)?,
+        ),
+    }
+}
+
+fn init_var<R: Rng + ?Sized, S: Into<Shape>>(
+    rng: &mut R,
+    init: init::Init,
+    s: S,
+    dtype: DType,
+    device: &Device,
+) -> Result<Var> {
+    use init::Init::*;
+    let shape = s.into();
+    Var::from_tensor(&match init {
+        Const(0.0) => Tensor::zeros(shape, dtype, device)?,
+        Const(1.0) => Tensor::ones(shape, dtype, device)?,
+        Const(v) => Tensor::ones(shape, dtype, device)?.affine(v, 0.0)?,
+        Uniform { lo, up } => rand_uniform_f64(rng, &shape, dtype, device, lo, up)?,
+        Randn { mean, stdev } => rand_normal_f64(rng, &shape, dtype, device, mean, stdev)?,
+        Kaiming {
+            dist,
+            fan,
+            non_linearity,
+        } => {
+            let fan = fan.for_shape(&shape);
+            let gain = non_linearity.gain();
+            let std = gain / (fan as f64).sqrt();
+            match dist {
+                NormalOrUniform::Uniform => {
+                    let bound = 3f64.sqrt() * std;
+                    rand_uniform_f64(rng, &shape, dtype, device, -bound, bound)?
+                }
+                NormalOrUniform::Normal => rand_normal_f64(rng, &shape, dtype, device, 0.0, std)?,
+            }
+        }
+    })
+}
+
+/// Determined [`VarMap`]
+#[derive(Clone)]
+pub struct DetVarMap<R: Rng + Send> {
+    inner: VarMap,
+    rng: Arc<Mutex<R>>,
+}
+
+impl<R: Rng + Send> DetVarMap<R> {
+    pub fn new(inner: VarMap, rng: R) -> Self {
+        Self {
+            inner,
+            rng: Arc::new(Mutex::new(rng)),
+        }
+    }
+}
+
+impl<R: Rng + Send> Deref for DetVarMap<R> {
+    type Target = VarMap;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+impl<R: Rng + Send> DerefMut for DetVarMap<R> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
+}
+
+impl<R: Rng + Send> SimpleBackend for DetVarMap<R> {
+    fn get(
+        &self,
+        shape: Shape,
+        name: &str,
+        init: init::Init,
+        dtype: DType,
+        device: &Device,
+    ) -> Result<Tensor> {
+        if !self.contains_tensor(name) {
+            let mut data = self.inner.data().lock().unwrap();
+            let mut rng = self.rng.try_lock().unwrap();
+            assert!(data
+                .insert(
+                    name.to_string(),
+                    init_var(rng.deref_mut(), init, &shape, dtype, device)?,
+                )
+                .is_none());
+        }
+        self.inner.get(shape, name, init, dtype, device)
+    }
+    fn contains_tensor(&self, name: &str) -> bool {
+        self.inner.contains_tensor(name)
+    }
+}
